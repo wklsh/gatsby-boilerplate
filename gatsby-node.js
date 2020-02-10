b7a73c86662354683ed8ba611f1eb2da9766a274
Shawn Lim Wee Kiang <wklsh@hotmail.sg>
2020-02-11 01:14:18 +0800

    feat: Improved createPages api example

--- gatsby-node.js
+++ gatsby-node.js
@@ -31,7 +31,8 @@ function generatePagePostSlugs(node, getNode, actions) {
 	const { createNodeField } = actions;
 
 	if (node.internal.type === "MarkdownRemark") {
-		const slug = createFilePath({ node, getNode, basePath: `CHANGE-THIS` });
+    // const slug = createFilePath({ node, getNode, basePath: `CHANGE-THIS` });
+		const slug = createFilePath({ node, getNode });
 
 		createNodeField({
 			node,
@@ -49,6 +50,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = async ({ graphql, actions }) => {
 	const { createPage } = actions;
 
+  // query for page parent directory as well to determine which templates to create with
 	const result = await graphql(`
 		query {
 			allMarkdownRemark {
@@ -57,6 +59,11 @@ exports.createPages = async ({ graphql, actions }) => {
 						fields {
 							slug
 						}
+						parent {
+							... on File {
+								relativeDirectory
+							}
+						}
 					}
 				}
 			}
@@ -64,9 +71,24 @@ exports.createPages = async ({ graphql, actions }) => {
 	`);
 
 	result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    let component;
+		switch (node.parent.relativeDirectory) {
+			case "DIR-ONE":
+				component = path.resolve(`./src/templates/REPLACE-ME-WITH-DIR-ONE-TEMP.jsx`);
+				break;
+
+			case "DIR-TWO":
+				component = path.resolve(`./src/templates/REPLACE-ME-WITH-DIR-TWO-TEMP.jsx`);
+				break;
+
+			default:
+        // null this when exmaple template is no longer needed
+				component = path.resolve(`./src/templates/BlogExample.jsx`);
+		}
+    
 		createPage({
 			path: `/work${node.fields.slug}`,
-			component: path.resolve(`./src/templates/BlogExample.jsx`),
+			component,
 			context: {
 				// Data passed to context is available in page queries, as GraphQL variables.
 				slug: node.fields.slug,
