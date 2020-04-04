2f6184d71805a89d5d481a1a52db8a8237f5c169
Shawn Lim Wee Kiang <wklsh@hotmail.sg>
2020-04-05 00:25:28 +0800

    feat: Add SEO component

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,16 +1,28 @@
 import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+import SEO from "../../components/SEO/SEO";
 
 import styles from "./Main.module.scss";
 
-export default class Main extends PureComponent {
+const propTypes = {
+	children: PropTypes.object.isRequired,
+};
+
+class Main extends PureComponent {
 	render() {
 		const { children } = this.props;
 
 		return (
-		  <main className={styles.main__wrapper}>
+			<main className={styles.main__wrapper}>
+				<SEO />
 				<h1 className="test">Main Layout</h1>
+
 				<section className={styles.children__wrapper}>{children}</section>
 			</main>
 		);
 	}
 }
+
+Main.propTypes = propTypes;
+export default Main;
